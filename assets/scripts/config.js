64165b763dd9fb3d7e6f6e13008b78ff04820b05
joseealejandro <joseealejandro@yahoo.com>
2017-08-26 19:33:26 -0400

    initial commit

---
+++ assets/scripts/config.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const config = {
+  apiOrigins: {
+    production: 'https://protected-scrubland-89743.herokuapp.com'
+  development: 'https://joseealejandro.github.io/table-music-2/'
+  }
+}
+
+module.exports = config
